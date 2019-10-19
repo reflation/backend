6b5dc7255735845bb11ad8ccae1a6a97508579a6
Muhun Kim <iam@muhun.kim>
2019-10-19 18:37:56 +0900

    feat: init check if user exist from mail server
    when signup. *mail server is webmail.jejunu.ac.kr
    TODO: check username from DB

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { TypeReq, TypeRes } from './@types/params'
 import { sendMail } from './mail'
 import { sign } from './jwt'
+import { isMailVail } from './auth'
 
 // Exported variable 'login' has or is using name 'e.Response' from external module "./node_modules/@types/express/index" but cannot be named in TS4023
-export const LoginRoute = async (req: TypeReq<string>, res: TypeRes) => {
-  const text = `localhost:3000/main&token=${await sign(req.body)}`
+export const LoginRoute = async ({ body }: TypeReq<string>, res: TypeRes) => {
+  // TODO: check username from DB
+  if (await isMailVail(body)) return res.send().status(403)
+  const text = `localhost:3000/main&token=${await create(body)}`
   console.log(text)
   await sendMail({ to: req.body, text })
   res.send().status(200)
