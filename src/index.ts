95fb238edbb825389f5a4bbbfef737f058b06e12
Muhun Kim <iam@muhun.kim>
2019-11-05 10:23:17 +0900

    chore: setup cors middleware for frontend

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
+import cors from 'cors'
 
 import { loginRoute, fetchRoute, cacheRoute } from './routes'
 import { ensureAuth } from './jwt'
 
 const app = express()
 
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
+app.use(cors({ origin: 'http://localhost:3000' }))
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 app.post('/login', bodyParser.json(), loginRoute)
